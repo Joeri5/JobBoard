83f6fdfc3a9f7ee5339c94d050a6c3b1498742fb
Joeri <joerischenk@outlook.com>
2022-11-27 16:23:51 +0100

    initial commit

---
+++ src/main/java/com/github/joeri5/jobboard/JobBoardApplication.java
@@ -0,0 +1,13 @@
+package com.github.joeri5.jobboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobBoardApplication.class, args);
+	}
+
+}
